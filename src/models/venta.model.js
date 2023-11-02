a0a2295f20535f4f49fca605d2a3e050a4594023
arturolongo <amizadayguapo@gmail.com>
2023-11-01 21:07:15 -0600

    última modificación de los modelos

--- src/models/venta.model.js
+++ src/models/venta.model.js
@@ -1,19 +1,32 @@
 import mongoose from "mongoose";
 
 const ventaSchema = new mongoose.Schema({
+
   vehiculo: {
+
     type: mongoose.Schema.Types.ObjectId,
+
     ref: "Vehiculo",
+
   },
+
   cliente: {
+
     type: mongoose.Schema.Types.ObjectId,
+
     ref: "Cliente",
   },
+
   vendedor: {
+
     type: mongoose.Schema.Types.ObjectId,
+
     ref: "Vendedor",
+
   },
+
   pago: Number,
+
   fechaVenta: Date,
 });
 
